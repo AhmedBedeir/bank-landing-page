f7004944664c322ef5439f0283aa4d9428ec02f0
AhmedBedeir <bederahmed0@gmail.com>
2024-10-04 22:32:55 +0300

    stats component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import styles from "./style";
-import { Navbar, Hero } from "./components";
+import { Navbar, Hero, Stats } from "./components";
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
+  <div className={`bg-primary w-full overflow-hidden`}>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
 
-    <div className={`${styles.flexStart} bg-primary`}>
+    <div className={`${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
     </div>
-    <div className={`${styles.flexStart} bg-primary`}>
-      <div className={`${styles.boxWidth}`}></div>
+    <div className={`${styles.flexCenter} ${styles.paddingX}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+      </div>
     </div>
-
   </div>
 );
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Hero from "./Hero";
 import Footer from "./Footer";
 import CardDeal from "./CardDeal";
 import FeedbackCard from "./FeedbackCard";
+import Stats from "./Stats";
 
-export { Navbar, Hero, Footer, CardDeal, FeedbackCard };
+export { Navbar, Hero, Footer, CardDeal, FeedbackCard, Stats };
